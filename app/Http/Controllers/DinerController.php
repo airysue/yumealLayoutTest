db524207ce601b698b27c45949103b75fc20ff91
Airy Sue <airysue@gmail.com>
2022-10-07 02:51:02 +0800

    比較整齊可讀的編輯圖檔功能版：比上一版本有稍整理過 code 並加上些說明

--- app/Http/Controllers/DinerController.php
+++ app/Http/Controllers/DinerController.php
@@ -9,8 +9,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 
-// use Intervention\Image\ImageManagerStatic as Image;
-
 class DinerController extends Controller
 {
   /**
@@ -51,72 +49,6 @@ class DinerController extends Controller
    * @return \Illuminate\Http\Response
    */
 
-  /*
-  public function store(Request $request)
-  {
-    //return $request->input();   exit;
-
-    //前面key區塊的名稱為表單名
-    //表格自建欄位 `din_no`, `din_name`, `din_intr`, `din_type`, `din_openTime`, `din_closeTime`, `din_addr`, `din_holiday`, `din_email`, `din_takeoutOnly`, `din_extraServiceFee`, `din_serviceFee`, `din_remark01`
-    $request->validate([
-      'din_photo' => 'required|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
-      //'file' => 'required|csv,txt,xlx,xls,pdf|max:2048', //一般檔案的寫法，還沒試是否能成功
-      'din_no' => 'required',
-      'din_name' => 'required'
-
-    ]);
-
-    $Diner = new Diner();
-
-    $Diner->din_name = request('din_name');  //$Diner->欄名 = request('表單名');
-    $Diner->din_no = request('din_no');
-    $Diner->din_type = json_encode(request('din_type'), JSON_UNESCAPED_UNICODE);
-    $Diner->din_intr = request('din_intr');
-
-    //處理時間欄位(http://static.kancloud.cn/kancloud/laravel-5-learning-notes/50163)
-    $Diner->din_openTime = Carbon::parse($request->input('din_openTime'))->toTimeString();
-    $Diner->din_closeTime = Carbon::parse($request->input('din_closeTime'))->toTimeString();
-
-    $Diner->din_tel = request('din_tel');
-    $Diner->din_addr = request('din_addr');
-    $Diner->din_holiday = request('din_holiday');
-    $Diner->din_email = request('din_email');
-
-    //用 enum 處理radio button
-    $Diner->din_takeoutOnly = request('din_takeoutOnly');
-    $Diner->din_extraServiceFee = request('din_extraServiceFee');
-    $Diner->din_serviceFee = request('din_serviceFee');
-    $Diner->user_id = Auth::user()->id;
-    $Diner->din_url01 = request('din_url01');
-    $Diner->din_remark01 = request('din_remark01');
-    $Diner->din_photo = request('din_photo');
-
-    //處理圖片上傳 start
-    $file = $request->hasFile('din_photo'); //先判斷圖片有沒有上傳成功
-    // dd($Diner->din_photo);dd($file);
-    if ($file) {
-      $requestData = $request->din_photo;
-      $fileName = time() . '-din_' . $request->file('din_photo')->getClientOriginalName();  //1664897691-din_期盼.png
-      $path = $request->file('din_photo')->storeAs('images', $fileName, 'public');  //照片已存入public\storage\images\1664897691-din_期盼.png
-      echo '$path=' . $path;  //印出  $path=images/1664897691-din_期盼.png
-      //dd($path);
-
-      $Diner->din_photo =  '/storage/' . $path;
-      //dd($Diner->din_photo);
-    } else {
-      return redirect()->route('Diner.index')->with('warning', "File upload error");
-    }
-    //處理圖片上傳 end
-
-    $Diner->save();  //存DB
-
-    //return redirect('/Diner)->with('mssg', '感謝填寫!');
-    return redirect('/Diner')->with('success', '新增資料成功'); //bootstrap alert
-
-  }
-   */
-
-  //重新改寫
   public function store(Request $request)
   {
     //return $request->input();   exit;
@@ -247,66 +179,25 @@ class DinerController extends Controller
    * @param  \App\Models\Diner  $Diner
    * @return \Illuminate\Http\Response
    */
-  /* 修改前
-  public function update(Request $request, $id)
-  {
-    $request->validate([
-      'din_no' => 'required',
-      'din_name' => 'required'
-
-    ]);
-
-    $Diner = Diner::findOrFail($id);
-    $Diner->update(
-      [
-        'din_no' => request('din_no'),
-        'din_name' => request('din_name'),
-        'din_type' => json_encode(
-          request('din_type'),
-          JSON_UNESCAPED_UNICODE
-        ),
-
-        'din_openTime' => request('din_openTime'),
-        'din_closeTime' => request('din_closeTime'),
-        'din_tel' => request('din_tel'),
-        'din_addr' => request('din_addr'),
-        'din_holiday' => request('din_holiday'),
-        'din_email' => request('din_email'),
-        'din_takeoutOnly' => request('din_takeoutOnly'),
-        'din_extraServiceFee' => request('din_extraServiceFee'),
-        'din_serviceFee' => request('din_serviceFee'),
-        'din_url01' => request('din_url01'),
-        'din_remark01' => request('din_remark01')
-
-      ]
-    );
-    return redirect('/Diner')->with('success', '更新資料成功');
-  }
- */
-
   public function update(Request $request, $id)
   {
 
     $Diner = Diner::findOrFail($id);
-    // $request->validate([
-    //   'din_no' => 'required',
-    //   'din_name' => 'required'
-    // ]);
-    $file = $request->file('din_photo');
-    $file_ready = $request->hasFile('din_photo');
+
 
     $file_name = $Diner->din_photo;
     //$file_path = $Diner->din_photo_path;
+    //資料庫的簡單連結路徑，並無法使用在 unlink()方法上，要想辦法將路徑弄成真實在硬碟上存在的路徑
+    //php無法正常顯示一條倒斜線，解決方法：利用兩個倒斜線來轉譯，頁面上看到的字串會變成一個倒斜線
+
     $file_path = public_path() . '\\storage\\images\\' . $Diner->din_photo;
-    echo '$file_path=' . $file_path;  //$file_path=images/1665040741-din_多行不義必自斃.png
-    //$file_path01 = public_path('\images' . $file_name);
-    echo '<br>$file_path01=' . $file_path;  //$file_path01=images/1665040741-din_多行不義必自斃.png
-    //exit;$file_path = app_path().'/images/news/'.$news->photo;
+    echo '$file_path=' . $file_path;
+    //會印出$file_path=V:\xampp8017\htdocs\laravel\yumealLayoutTest\public\storage\images\1665041647-din_多行不義必自斃.png
+    exit;
 
-    if ($request->hasFile('din_photo')) {
+    if ($request->hasFile('din_photo')) {  //如果本來就有檔案存在
 
-
-      unlink($file_path);
+      unlink($file_path); //字面上看似是解除與該檔案的連結，實際上實體的檔案也會被刪掉
 
       $new_upload = $request->file('din_photo');
       $file_name = time() . '-din_' . $request->file('din_photo')->getClientOriginalName();
@@ -314,43 +205,15 @@ class DinerController extends Controller
       $file_path = $request->file('din_photo')->storeAs('images', $file_name, 'public');
 
       $Diner->din_photo = $file_name;
-      $Diner->din_photo_path =  '/storage/' . $file_path; //檔名連同圖檔所在資料夾路徑
-      echo 'here i am__' . $file_name;
+      $Diner->din_photo_path =  '/storage/' . $file_path; //public之後的檔名路徑，檔名連同圖檔所在資料夾路徑
+      echo '<br>$file_name=' . $file_name;
       echo '<br>$Diner->din_photo=' . $Diner->din_photo;
-      echo '<br>$Diner->din_photo_path=' . $Diner->din_photo_path;
-      //exit;
-    } else {
+      echo '<br>$Diner->din_photo_path=' . $Diner->din_photo_path; //exit;
+
+    } else {  //如果沒有新的檔案被傳入，就顯示原來舊有的檔案資料
       $Diner->din_photo = $request->ori_din_photo;
     }
     //dd($file_path);
-
-    // $Diner->din_name = request('din_name');  //$Diner->欄名 = request('表單名');
-    // $Diner->din_no = request('din_no');
-    // $Diner->din_type = json_encode(request('din_type'), JSON_UNESCAPED_UNICODE);
-    // $Diner->din_intr = request('din_intr');
-
-    // //處理時間欄位(http://static.kancloud.cn/kancloud/laravel-5-learning-notes/50163)
-    // $Diner->din_openTime = Carbon::parse($request->input('din_openTime'))->toTimeString();
-    // $Diner->din_closeTime = Carbon::parse($request->input('din_closeTime'))->toTimeString();
-
-    // $Diner->din_tel = request('din_tel');
-    // $Diner->din_addr = request('din_addr');
-    // $Diner->din_holiday = request('din_holiday');
-    // $Diner->din_email = request('din_email');
-
-    // //用 enum 處理radio button
-    // $Diner->din_takeoutOnly = request('din_takeoutOnly');
-    // $Diner->din_extraServiceFee = request('din_extraServiceFee');
-    // $Diner->din_serviceFee = request('din_serviceFee');
-    // $Diner->user_id = Auth::user()->id;
-    // $Diner->din_url01 = request('din_url01');
-    // $Diner->din_remark01 = request('din_remark01');
-    // $Diner->din_photo = request('din_photo');
-    // //$Diner->din_photo = $file_name; //只存檔名
-
-
-
-
 
 
     $Diner->update(
@@ -372,14 +235,14 @@ class DinerController extends Controller
         'din_extraServiceFee' => request('din_extraServiceFee'),
         'din_serviceFee' => request('din_serviceFee'),
         'din_url01' => request('din_url01'),
-        'din_photo' => request('din_photo'),
+        'din_photo' => $Diner->din_photo,
         'din_photo_path' => $Diner->din_photo_path,
         'din_remark01' => request('din_remark01')
 
       ]
     );
     //dd($Diner);
-    /**/
+
     return redirect('/Diner')->with('success', '更新資料成功');
   }
 
@@ -389,6 +252,16 @@ class DinerController extends Controller
    * @param  \App\Models\Diner  $Diner
    * @return \Illuminate\Http\Response
    */
+
+  /*
+  public function destroy($id)
+  {
+    $Diner = Diner::findOrFail($id);
+    $Diner->delete();
+    return redirect(url('/Diner'))->with('success', '刪除資料成功');
+  }
+ */
+  //重新調整刪除功能
   public function destroy($id)
   {
     $Diner = Diner::findOrFail($id);
